f5eb7fe09cd4bd7cd341bd814ee8a3148c0ead11
gray-harrison-delacon <gray.harrison@delaconcorp.com>
2025-05-22 10:29:30 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,3 +30,8 @@ io.on("connection", (socket) => {
 });
 
 server.listen(4000, () => console.log("Server running on port 4000"));
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(`Server running on port ${4000}`);
+});
